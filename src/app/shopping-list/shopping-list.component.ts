0da5ae7905e3f2c85c710cb917d8cc89a87a8672
tusharb <tusharbansal@iitbhilai.ac.in>
2023-05-26 10:37:15 +0530

    first commit

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent {
+    ingredients: Ingredient[]=[
+      new Ingredient('Apple',10),
+      new Ingredient('Orange',5)
+    ];
+
+}
